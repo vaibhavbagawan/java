9ba2c4f28e83316d664e612492d74ddbfb5a32b7
vaibhav bagawan <vaibhavbagawan@gmail.com>
2025-07-22 17:50:58 +0530

    1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,5 @@
+// in this code, we just print a hello world string.
+
 public class HelloWorld{
   public static void main(String[] args){
     System.out.println("Hello World!");
---
+++ sumOfTwoNumbers.java
@@ -0,0 +1,11 @@
+/*
+this is a code in which we create two variables and then print the sum of them.
+*/
+
+public class sumOfTwoNumbers {
+    public static void main(String[] args) {
+    int firstNumber = 2;
+    int secondNumber = 3;
+    System.out.println("sum: "+(firstNumber+secondNumber));
+    }
+}
