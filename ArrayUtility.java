3f3ce953aab4db4e1c1c0f77691149940bb6b728
vaibhav bagawan <vaibhavbagawan@gmail.com>
2025-07-23 17:20:34 +0530

    1

---
+++ ArrayUtility.java
@@ -0,0 +1,23 @@
+//this code is used to create array and take values from the user.
+
+import java.util.Scanner;
+
+public class ArrayUtility{
+    public static int[] takeArray(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter the number of element: ");
+        int num = input.nextInt();
+
+        int[] myArr = new int[num];
+
+        int i = 0;
+        while(i<num){
+            System.out.print("Please enter element no "+(i+1)+": ");
+            myArr[i]=input.nextInt();
+            i++;
+        }
+
+        input.close();
+        return myArr;
+    }
+}
---
+++ Challenge40.java
@@ -0,0 +1,29 @@
+// sum and average
+
+public class Challenge40 {
+    public static void main(String[] args){
+        int[] array = ArrayUtility.takeArray();
+
+        System.out.println("Sum of element: "+sum(array));
+        System.out.print("Average of element: "+average(array));
+    }
+    public static int sum(int[] array){
+        int i = 0;
+        int sum = 0;
+        while(i<array.length) {
+            sum += array[i];
+            i++;
+        }
+        return sum;
+    }
+    public static double average(int[] array){
+        int i = 0;
+        int sum = 0;
+        while(i<array.length) {
+            sum += array[i];
+            i++;
+        }
+        double average = sum/(double)array.length;
+        return average;
+    }
+}
