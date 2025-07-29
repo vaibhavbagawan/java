c7bd4c67415cd51f936a467589da688926735a07
vaibhav bagawan <vaibhavbagawan@gmail.com>
2025-07-30 00:08:23 +0530

    1

---
+++ Challenge49.java
@@ -0,0 +1,33 @@
+//this code is used ot find the sum and average of the 2-D array
+public class Challenge49 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the average code of sum to find the");
+
+        int[][] array2d = ArrayUtility.take2dArray();
+
+        double sum = sum(array2d);
+        System.out.println("Sum of the 2-D array is: "+sum);
+        double average = average(array2d);
+        System.out.println("the average or array is: "+average);
+    }
+
+    public static double sum(int[][] array2d){
+        double sum = 0;
+        int i = 0;
+        while(i <array2d.length){
+            int j = 0;
+            while(j<array2d[0].length){
+               sum+=array2d[i][j];
+                j++;
+            }
+            i++;
+        }
+        return sum;
+    }
+
+    public static double average(int[][] array2d){
+        double sum = sum(array2d);
+        double average = sum/(array2d.length+array2d[0].length);
+        return average;
+    }
+}
